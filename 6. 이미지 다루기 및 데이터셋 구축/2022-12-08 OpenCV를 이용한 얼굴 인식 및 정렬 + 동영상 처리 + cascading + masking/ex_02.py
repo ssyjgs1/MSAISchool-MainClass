f571aad6cd24f1ed15c31ab3d1251202cefbf8f6
SEUNGYOON LEE <ssyjgs1@outlook.com>
2022-12-09 00:37:22 +0900

    delete section of homework

--- 6. 이미지 다루기 및 데이터셋 구축/2022-12-08 OpenCV를 이용한 얼굴 인식 및 정렬 + 동영상 처리 + cascading + masking/ex_02.py
+++ 6. 이미지 다루기 및 데이터셋 구축/2022-12-08 OpenCV를 이용한 얼굴 인식 및 정렬 + 동영상 처리 + cascading + masking/ex_02.py
@@ -4,13 +4,13 @@ import numpy as np
 # ex-01
 img_rectangle = np.ones((400,400), dtype='uint8')
 cv2.rectangle(img_rectangle, (50,50), (300,300), (255,255,255), -1)
-cv2.imshow('image show', img_rectangle)
+cv2.imshow('image rectangle', img_rectangle)
 cv2.waitKey(0)
 
 # ex-02
 img_circle = np.ones((400,400), dtype='uint8')
 cv2.circle(img_circle,(300,300), 70, (255,255,255), -1)
-cv2.imshow('image show', img_circle)
+cv2.imshow('image circle', img_circle)
 cv2.waitKey(0)
 
 # ex-03
@@ -27,11 +27,11 @@ cv2.imshow('image bitwiseXor', bitwiseXor)
 cv2.waitKey(0)
 
 recNot = cv2.bitwise_not(img_rectangle)
-cv2.imshow('image bitwiseNot', recNot)
+cv2.imshow('image recnot bitwiseNot', recNot)
 cv2.waitKey(0)
 
 circleNot = cv2.bitwise_not(img_circle)
-cv2.imshow('image bitwiseNot', circleNot)
+cv2.imshow('image circlenot bitwiseNot', circleNot)
 cv2.waitKey(0)
 
 # ex-04 마스킹(하얀 빈 공간에 이미지 넣기 - 과제)
@@ -39,22 +39,6 @@ mask = np.zeros((683,1024), dtype='uint8')
 rec1=cv2.rectangle(mask,(60,50),(280,280), (255,255,255), -1)
 rec2=cv2.rectangle(mask, (420,50),(550,230), (255,255,255), -1)
 rec3=cv2.rectangle(mask, (750,50),(920,280), (255,255,255), -1)
-cv2.imwrite('./mask.png', mask)
-mask = cv2.imread('./mask.png')
 
-p1 = cv2.imread('./p1.jpg')
-small_p1 = cv2.resize(p1,(220,230))
-p2 = cv2.imread('./p2.jpeg')
-small_p2 = cv2.resize(p2,(130,180))
-p3 = cv2.imread('./p3.png')
-small_p3 = cv2.resize(p3,(170,230))
-
-p1x_offset = 60
-p1y_offset = 50
-
-p1x_end = p1x_offset + small_p1.shape[0]
-p1y_end = p1y_offset + small_p1.shape[1]
-mask[p1y_offset:p1y_end, p1x_offset:p1x_end] = small_p1
-
-cv2.imshow('...', mask)
+cv2.imshow('Mask', mask)
 cv2.waitKey(0)
