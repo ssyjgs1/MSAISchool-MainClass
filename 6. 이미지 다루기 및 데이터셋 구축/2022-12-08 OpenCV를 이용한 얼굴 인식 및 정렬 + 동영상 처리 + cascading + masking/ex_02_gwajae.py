fcac62e410fedc48f716e06aadfeb7ce60ee4afb
SEUNGYOON LEE <ssyjgs1@outlook.com>
2022-12-09 19:49:20 +0900

    new image source

--- 6. 이미지 다루기 및 데이터셋 구축/2022-12-08 OpenCV를 이용한 얼굴 인식 및 정렬 + 동영상 처리 + cascading + masking/ex_02_gwajae.py
+++ 6. 이미지 다루기 및 데이터셋 구축/2022-12-08 OpenCV를 이용한 얼굴 인식 및 정렬 + 동영상 처리 + cascading + masking/ex_02_gwajae.py
@@ -2,26 +2,26 @@
 import cv2
 import numpy as np
 
-drx = cv2.imread('./drx.png')
+drx = cv2.imread('./drx.png') # 원본 이미지 불러오기
 cv2.imshow('DRX', drx)
 cv2.waitKey(0)
 
 face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 
-drx_gray = cv2.cvtColor(drx, cv2.COLOR_BGR2GRAY)
+drx_gray = cv2.cvtColor(drx, cv2.COLOR_BGR2GRAY) # 얼굴 찾기 해주기 위해 그레이스케일로 변환
 cv2.imshow('DRX Gray', drx_gray)
 cv2.waitKey(0)
 
 faces = face_cascade.detectMultiScale(drx_gray, 1.1, 4)
 
-face_crop = []
-for (x, y, w, h) in faces :
+face_crop = [] # 빈 공간 생성
+for (x, y, w, h) in faces : # 얼굴 정보 찾은 거 상기 생성한 빈 공간에 추가
     face_crop.append(drx[y:(y+h), x:(x+w)])
-    cv2.rectangle(drx, (x,y), (x+w, y+h), (10,100,250), thickness=2)
+    cv2.rectangle(drx, (x,y), (x+w, y+h), (10,100,250), thickness=2) # 원본 그림에 객체 찾은 거 추가
 cv2.imshow('DRX-face-line', drx)
 cv2.waitKey(0)
 
-# 과제 박스
+# 과제 구간
 mask = np.zeros((683, 1024, 3), dtype='uint8') 
 cv2.rectangle(mask,(60,50),(280,280), (255,255,255), -1)
 cv2.rectangle(mask, (420,50),(550,230), (255,255,255), -1)
@@ -29,7 +29,7 @@ cv2.rectangle(mask, (750,50),(920,280), (255,255,255), -1)
 cv2.imshow('Original Mask', mask)
 cv2.waitKey(0)
 
-x_offset, y_offset, x_end, y_end, resize_face_crop = [60,420,750], [50,50,50], [280,550,920], [280,230,280], []
+x_offset, y_offset, x_end, y_end, resize_face_crop = [60,420,750], [50,50,50], [280,550,920], [280,230,280], [] # 오프셋, 끝점, 사이즈 변경 빈공간 생성
 
 for i in range(len(face_crop)) :
     resize_face_crop.append(cv2.resize(face_crop[i], (x_end[i] - x_offset[i], y_end[i] - y_offset[i])))
