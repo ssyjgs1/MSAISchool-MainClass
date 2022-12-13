16ef0dd9f05a37c288303895efb37ed72e4d28ea
SEUNGYOON LEE <ssyjgs1@outlook.com>
2022-12-13 13:50:54 +0900

    edit code structure

--- 7. 데이터 증강 라벨링, 이미지 처리, 데이터셋 구현, 데이터 가공 프로젝트/2022-12-13/coco_seg.py
+++ 7. 데이터 증강 라벨링, 이미지 처리, 데이터셋 구현, 데이터 가공 프로젝트/2022-12-13/coco_seg.py
@@ -1,45 +1,43 @@
 import os
 import json
 import cv2
+import numpy as np
 
-json_path = 'annotations/instances_default.json'
-with open(json_path, "r") as f :
+
+json_path = "./annotations/instances_default.json"
+
+with open(json_path, "r") as f:
     coco_info = json.load(f)
-# print(coco_info)
-
-
-# 파일을 읽기 실패했을 때
+# 파일 읽기 실패
 assert len(coco_info) > 0, "파일 읽기 실패"
 
 
-# category 정보
+# 카테고리 수집
 categories = dict()
-for category in coco_info['categories'] :
+for category in coco_info['categories']:
     categories[category["id"]] = category["name"]
-# print(categories)
 
 
 # annotation 정보
 ann_info = dict()
-for annotation in coco_info['annotations'] :
-    image_id = annotation['image_id']
+for annotation in coco_info['annotations']:
+    image_id = annotation["image_id"]
     bbox = annotation["bbox"]
     category_id = annotation["category_id"]
     segmentation = annotation["segmentation"]
-    # print(image_id, category_id, bbox, segmentation)
 
-    if image_id not in coco_info["images"] :
+    if image_id not in ann_info:
         ann_info[image_id] = {
-            "boxes" : [bbox],
-            "segmentation" : [segmentation],
-            "categories" : [category_id] 
+            "boxes": [bbox],
+            "segmentation": [segmentation],
+            "categories": [category_id]
         }
-    else :
+    else:
         ann_info[image_id]["boxes"].append(bbox)
         ann_info[image_id]["segmentation"].append(segmentation)
         ann_info[image_id]["categories"].append(categories[category_id])
 
-for image_info in coco_info["images"] :
+for image_info in coco_info["images"]:
     filename = image_info['file_name']
     width = image_info['width']
     height = image_info['height']
@@ -48,14 +46,17 @@ for image_info in coco_info["images"] :
     file_path = os.path.join("./images", filename)
     img = cv2.imread(file_path)
 
-    try :
+    try:
         annotation = ann_info[img_id]
-    except KeyError :
+    except KeyError:
         continue
 
-    for bbox, segmentation, category in zip(annotation['boxes'], annotation['segmentation'], annotation['categories']) :
+    for bbox, segmentation, category in zip(annotation['boxes'], annotation['segmentation'], annotation['categories']):
         x1, y1, w, h = bbox
-        for seg in segmentation :
-            import numpy as np
+        for seg in segmentation:
+            # print(seg)
             poly = np.array(seg, np.int32).reshape((int(len(seg)/2), 2))
             print(poly)
+            poly_img = cv2.polylines(img, [poly], True, (255, 0, 0), 2)
+    cv2.imshow("test", poly_img)
+    cv2.waitKey(0)
