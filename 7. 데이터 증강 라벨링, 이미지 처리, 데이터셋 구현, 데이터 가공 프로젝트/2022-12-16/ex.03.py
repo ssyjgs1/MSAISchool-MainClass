5b2bf50ebb89b6aaa3f39bdb627f00d9263ae513
SEUNGYOON LEE <ssyjgs1@outlook.com>
2022-12-16 17:50:30 +0900

    initial commit

---
+++ 7. 데이터 증강 라벨링, 이미지 처리, 데이터셋 구현, 데이터 가공 프로젝트/2022-12-16/ex.03.py
@@ -0,0 +1,26 @@
+import cv2
+import random
+import albumentations as A
+
+# define a function to visualize an image
+def visualize(image) :
+    cv2.imshow('visualization', image)
+    cv2.waitKey(0)
+
+# load a image from disk
+image = cv2.imread('./ressha.jpg')
+
+# visualize the original image
+# visualize(image)
+
+# random rain
+transform = A.Compose([
+    A.RandomRain(brightness_coefficient=0.7, drop_width=1, blur_value=3, p=1),
+    A.RandomSnow(brightness_coeff=2.2 ,snow_point_lower=0.3, snow_point_upper=0.3, p=1),
+    A.RandomSunFlare(flare_roi=(0,0,1,0.5), angle_lower=0.3, p=1),
+    A.RandomShadow(num_shadows_lower=3, num_shadows_upper=3, shadow_dimension=5, shadow_roi=(0,0.5,1,1), p=1),
+    A.RandomFog(fog_coef_lower=0.7, fog_coef_upper=0.8, alpha_coef=0.1, p=1)
+])
+
+transformed = transform(image=image)
+visualize(transformed["image"])
